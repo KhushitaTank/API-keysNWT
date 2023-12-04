c17bb49603d8bd4b9d69a2c7d79477f9777794df
Khushita Tank <khushita.tank@gmail.com>
2023-12-04 07:05:03 +0530

    first commit

--- Routes/movieGenre.js
+++ Routes/movieGenre.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const needle = require("needle");
 
-const API_KEY_GENER = process.env.API_KEY_GENER;
+const API_KEY_GENRE = process.env.API_KEY_GENRE;
 
 router.get("/", async (req, res) => {
   try {
-    const apiRes = await needle("get", `${API_KEY_GENER}`);
+    const apiRes = await needle("get", `${API_KEY_GENRE}`);
     const data = apiRes.body;
     res.status(200).json(data);
   } catch (error) {
